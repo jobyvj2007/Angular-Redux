9f634b2ecfcfac7f3c7ddba2bb069c0f9ccbeb06
Joby Joseph <jobyvj2009@gmail.com>
2017-09-29 18:58:28 +0530

    Update auto-complete.component.ts

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bc-auto-complete',
-  templateUrl: './auto-complete.component.html',
-  styleUrls: ['./auto-complete.component.css']
+  templateUrl: './auto-complete.component.html'
 })
 export class AutoCompleteComponent implements OnInit {
 
